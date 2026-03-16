6a2c40d9c5ee01cc7a30dd2e6e1b4df2f0563f6d
Alfonsus Ardani <alfonsusac@gmail.com>
2026-03-17 00:20:52 +0700

    changelog

--- changelog.ts
+++ changelog.ts
@@ -1,8 +1,14 @@
 export const changelog = {
+  "0.0.6": {
+    "date": "2026-03-16T17:20:22.370Z",
+    "changes": [
+      "Test reading package.json from the host"
+    ]
+  },
   "0.0.5": {
     "date": "2026-03-16T15:43:32.588Z",
     "changes": [
-      "Improve building and publishing process",
+      "Improve building and publishing process"
     ]
   }
 } satisfies Record<string, {
