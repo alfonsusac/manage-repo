d210aa198a2c452734998d0ac2165d7f80643fbb
Alfonsus Ardani <alfonsusac@gmail.com>
2026-03-18 20:42:25 +0700

    update meta

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -16,83 +16,113 @@ import { logger, type ChangelogFile } from "./util"
 const skipGitCheck = process.argv.includes("--skip-git-check")
 const log = logger("publish")
 
-
 log("Preparing to publish ManageRepo...")
 
-// 1. Check if tests are passing
-// log("Running tests...")
-// await $`bun test`
-//   .catch(error => {
-//     console.error("Tests failed. Please fix test errors before publishing.")
-//     process.exit(1)
-//   })
+const errors: string[] = []
 
-// 2. Check if build is working
-log("Building the project...")
-await $`bun run build`.quiet()
-  .catch(error => {
-    console.error("Build failed. Please fix build errors before publishing.")
-    process.exit(1)
-  })
-
-// 3. Check if git is initialized and has a remote
-log("Checking git status...")
-await $`git rev-parse --is-inside-work-tree`
-  .text()
-  .then(res => {
-    if (res.trim() === "true" || skipGitCheck) return
-    console.error("Git is not initialized. Please run `git init` and add a remote before publishing.")
-    process.exit(1)
-  })
-await $`git status --porcelain`
-  .text()
-  .then(res => {
-    if (res.trim() === "" || skipGitCheck) return
-    console.error("Git has uncommitted changes. Please commit or stash changes before publishing.")
-    process.exit(1)
-  })
-await $`git remote`
-  .text()
-  .then(res => {
-    if (res.trim() || skipGitCheck) return
-    console.error("Git has no remote. Please add a remote before publishing.")
-    process.exit(1)
-  })
-await $`git log --branches --not --remotes`
-  .text()
-  .then(res => {
-    if (res.trim() === "" || skipGitCheck) return
-    console.error("Current commit is not pushed to remote. Please push changes before publishing.")
-    process.exit(1)
-  })
-
-// 4. Check if current version exists in npm
-log("Checking npm version...")
+// Files
 const packageJson = await import("../package.json")
-const npmInfo = await $`npm view manage-repo version`.quiet()
-if (npmInfo.text().trim() === packageJson.version) {
-  console.error(`Version ${ packageJson.version } already exists in npm. Please run \`bun version <minor|major|patch>\` to update the version before publishing.`)
-  process.exit(1)
-}
-
-// 5. Check if changelog.ts has entry for current version
-log("Checking changelog...")
 const { changelog } = await import("../changelog.ts" as string) as { changelog: ChangelogFile }
-if (!changelog[ packageJson.version ]) {
-  console.error(`Changelog does not have an entry for version ${ packageJson.version }. Please add an entry to changelog.ts before publishing.`)
-  process.exit(1)
-}
-if (!changelog[ packageJson.version ]?.changes?.length) {
-  console.error(`Changelog entry for version ${ packageJson.version } is empty. Please add changes to the changelog entry before publishing.`)
-  process.exit(1)
-}
 
-// 6. Check if npm is logged on
+// 1. Check if tests are passing
+// TODO: add test check before publish
+
+log("Checking bin path...")
+await checkBinPath()
+
+log("Building the project...")
+await checkBuild()
+
+log("Checking git status...")
+await checkGitStatus()
+
+log("Checking npm version...")
+await checkIfCurrentVersionExistsInNpm()
+
+log("Checking changelog...")
+await checkIfChangelogHasEntry()
+
 log("Checking npm login status...")
-await $`npm whoami`.quiet()
-  .catch(error => {
-    console.error("Not logged in to npm. Please run `npm login` before publishing.")
-    process.exit(1)
-  })
+await checkIfNpmIsLoggedOn()
+
+if (errors.length) {
+  console.error(`Errors found (${errors.length}):`)
+  errors.forEach((error, index) => console.error(`${index + 1}) ${ error }`))
+  console.error("Please fix the above errors before publishing.")
+  process.exit(1)
+}
 
 log("All checks passed. Ready to publish!")
+
+
+
+
+async function checkBinPath() {
+  if (packageJson.bin) {
+    for (const [ name, path ] of Object.entries(packageJson.bin)) {
+      const exists = await Bun.file(path).exists()
+      if (!exists)
+        errors.push(`Bin path ${ path } for ${ name } does not exist. Please fix the bin path in package.json before publishing.`)
+    }
+  }
+}
+
+async function checkBuild() {
+  await $`bun run build`.quiet()
+    .catch(error => {
+      errors.push("Build failed. Please fix build errors before publishing.")
+    })
+}
+
+async function checkGitStatus() {
+  await $`git rev-parse --is-inside-work-tree`
+    .text()
+    .then(res => {
+      if (res.trim() === "true" || skipGitCheck) return
+      errors.push("Git is not initialized. Please run `git init` and add a remote before publishing.")
+    })
+  await $`git status --porcelain`
+    .text()
+    .then(res => {
+      if (res.trim() === "" || skipGitCheck) return
+      errors.push("Git has uncommitted changes. Please commit or stash changes before publishing.")
+    })
+  await $`git remote`
+    .text()
+    .then(res => {
+      if (res.trim() || skipGitCheck) return
+      errors.push("Git has no remote. Please add a remote before publishing.")
+    })
+  await $`git log --branches --not --remotes`
+    .text()
+    .then(res => {
+      if (res.trim() === "" || skipGitCheck) return
+      errors.push("Current commit is not pushed to remote. Please push changes before publishing.")
+    })
+}
+
+async function checkIfCurrentVersionExistsInNpm() {
+  const npmInfo = await $`npm view manage-repo version`.quiet()
+  if (npmInfo.text().trim() === packageJson.version) {
+    errors.push(`Version ${ packageJson.version } already exists in npm. Please run \`bun version <minor|major|patch>\` to update the version before publishing.`)
+  }
+}
+
+async function checkIfChangelogHasEntry() {
+  if (!changelog[ packageJson.version ]) {
+    errors.push(`Changelog does not have an entry for version ${ packageJson.version }. Please add an entry to changelog.ts before publishing.`)
+    return
+  }
+  if (!changelog[ packageJson.version ]?.changes?.length) {
+    errors.push(`Changelog entry for version ${ packageJson.version } is empty. Please add changes to the changelog entry before publishing.`)
+    return
+  }
+}
+
+async function checkIfNpmIsLoggedOn() {
+  try {
+    await $`npm whoami`.quiet()
+  } catch (error) {
+    errors.push("Not logged in to npm. Please run `npm login` before publishing.")
+  }
+}
